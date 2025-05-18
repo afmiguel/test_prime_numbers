bcc2076b8ca7325491b583e6ef44c4d4eb8e9ced
afonsomiguel <afonso.miguel@gmail.com>
2025-05-18 17:02:18 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use primal::is_prime;
+use std::time::Instant;
+
+const MAX_VALUE: u64 = 200_000_000;
+const NUM_THREADS: usize = 10;
+
+fn main() {
+    println!("Sequential Execution");
+    let start = Instant::now();
+    let mut count_primes = 0;
+    for n in 0..MAX_VALUE{
+        if is_prime(n) {
+            count_primes += 1;
+        }
+    }
+    let duration = start.elapsed();
+    println!("There are {} primes", count_primes);
+    println!("Time taken: {:?}", duration);
+}
